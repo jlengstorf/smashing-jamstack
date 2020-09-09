5e14d0fafa5f70471a65bf4e4dd5f1517c7751a9
Jason Lengstorf <jason@lengstorf.com>
2020-09-08 22:20:55 -0700

    feat: boop button

--- functions/boop.js
+++ functions/boop.js
@@ -1,6 +1,47 @@
-exports.handler = async () => {
+const fetch = require('node-fetch');
+
+async function sendQuery({ query, variables = {} }) {
+  return await fetch('https://graphql.fauna.com/graphql', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
+    },
+    body: JSON.stringify({ query, variables }),
+  });
+}
+
+exports.handler = async (event) => {
+  const body = event.body ? JSON.parse(event.body) : {};
+
+  if (body.add) {
+    await sendQuery({
+      query: `
+        mutation ($time: Time!) {
+          createBoop(data: { time: $time }) {
+            time
+          }
+        }
+      `,
+      variables: { time: new Date().toISOString() },
+    });
+  }
+
+  const count = await sendQuery({
+    query: `
+      query {
+        allBoops {
+          data {
+            _id
+          }
+        }
+      }
+    `,
+  })
+    .then((response) => response.json())
+    .then((result) => result.data.allBoops.data.length);
+
   return {
     statusCode: 200,
-    body: 'boop!',
+    body: JSON.stringify(count),
   };
 };
