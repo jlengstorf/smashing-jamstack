7d0f320642784349dae6d6f3a979d434ce98b788
Jason Lengstorf <jason@lengstorf.com>
2020-09-09 07:40:54 -0700

    chore: debugging

--- functions/sign-guestbook.js
+++ functions/sign-guestbook.js
@@ -1,7 +1,9 @@
 const { getSignatures, addSignature } = require('./util/fauna');
 
 exports.handler = async (_event, context) => {
+  console.log(context.clientContext);
   const { user } = context.clientContext;
+  console.log(user);
 
   if (!user || !user.app_metadata) {
     return {
