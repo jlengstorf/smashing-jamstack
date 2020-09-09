29bae8a1e58da977c496de1f82f0e421caf86c22
Jason Lengstorf <jason@lengstorf.com>
2020-09-09 09:47:21 -0700

    feat: boop

---
+++ functions/util/fauna.js
@@ -0,0 +1,45 @@
+const fetch = require('node-fetch');
+
+async function sendQuery({ query, variables = {} }) {
+  return await fetch('https://graphql.fauna.com/graphql', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
+    },
+    body: JSON.stringify({ query, variables }),
+  }).then((response) => response.json());
+}
+
+async function getBoopCount() {
+  const result = await sendQuery({
+    query: `
+      query {
+        allBoops {
+          data {
+            time
+          }
+        }
+      }
+    `,
+  });
+
+  const boopCount = result.data.allBoops.data.length;
+
+  return boopCount;
+}
+
+async function addBoop() {
+  await sendQuery({
+    query: `
+      mutation ($time: Time!) {
+        createBoop(data: { time: $time }) {
+          time
+        }
+      }
+    `,
+    variables: { time: new Date().toISOString() },
+  });
+}
+
+exports.getBoopCount = getBoopCount;
+exports.addBoop = addBoop;
