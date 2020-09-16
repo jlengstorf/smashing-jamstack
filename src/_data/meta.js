176b865b3d9d2fba788f4150d736620876db6b89
Jason Lengstorf <jason@lengstorf.com>
2020-09-15 20:38:50 -0700

    feat: initial setup

---
+++ src/_data/meta.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // keep it short! shown in the header
+  title: 'Smashing Jamstack',
+
+  // these are all optional and add links to the footer
+  repo: 'jlengstorf/smashing-jamstack',
+  // episode: '',
+  // tutorial: '',
+};
