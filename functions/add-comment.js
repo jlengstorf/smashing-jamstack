74bc3b65187bc47c539f141e8fb88770d878df66
Jason Lengstorf <jason@lengstorf.com>
2020-09-15 22:05:00 -0700

    feat: live data

---
+++ functions/add-comment.js
@@ -0,0 +1,40 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event, context) => {
+  const { movie_id, text } = JSON.parse(event.body);
+  const { user } = context.clientContext;
+
+  if (!user) {
+    return {
+      statusCode: 401,
+      body: 'Unauthorized',
+    };
+  }
+
+  const data = await fetch('https://lasting-snail-67.hasura.app/v1/graphql', {
+    method: 'POST',
+    headers: {
+      'X-Hasura-Role': 'user',
+      'X-Hasura-User-ID': user.sub,
+    },
+    body: JSON.stringify({
+      query: `
+          mutation ($movie: uuid! $text: String!, $user: String!) {
+          insert_comments(objects: {movie_id: $movie, text: $text, user_id: $user}) {
+            affected_rows
+          }
+        }
+      `,
+      variables: {
+        movie: movie_id,
+        text,
+        user: user.sub,
+      },
+    }),
+  }).then((response) => response.json());
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+};
--- src/_data/movies.js
+++ src/_data/movies.js
@@ -1,30 +1,28 @@
-module.exports = [
-  {
-    id: 1,
-    title: 'Test 1',
-    poster: 'https://jason.af/fem/img/batman-v-booperman.jpg',
-    body:
-      'Donec enim set elit donec donec neque eu donec. Fritos elementum gravida adipiscing, sem lacus feugiat convallis nub donec. Tellus mattis gravida, set vel neque ipsum donec ligula donec sem viverra In lacus.',
-  },
-  {
-    id: 2,
-    title: 'Test 2',
-    poster: 'https://jason.af/fem/img/batman-v-booperman.jpg',
-    body:
-      'Donec enim set elit donec donec neque eu donec. Fritos elementum gravida adipiscing, sem lacus feugiat convallis nub donec. Tellus mattis gravida, set vel neque ipsum donec ligula donec sem viverra In lacus.',
-  },
-  {
-    id: 3,
-    title: 'Test 3',
-    poster: 'https://jason.af/fem/img/batman-v-booperman.jpg',
-    body:
-      'Donec enim set elit donec donec neque eu donec. Fritos elementum gravida adipiscing, sem lacus feugiat convallis nub donec. Tellus mattis gravida, set vel neque ipsum donec ligula donec sem viverra In lacus.',
-  },
-  {
-    id: 4,
-    title: 'Test 4',
-    poster: 'https://jason.af/fem/img/batman-v-booperman.jpg',
-    body:
-      'Donec enim set elit donec donec neque eu donec. Fritos elementum gravida adipiscing, sem lacus feugiat convallis nub donec. Tellus mattis gravida, set vel neque ipsum donec ligula donec sem viverra In lacus.',
-  },
-];
+const fetch = require('node-fetch');
+
+module.exports = async () => {
+  const { data } = await fetch(
+    'https://lasting-snail-67.hasura.app/v1/graphql',
+    {
+      method: 'POST',
+      headers: {
+        'X-Hasura-Role': 'public',
+        'X-Hasura-Admin-Secret': process.env.HASURA_ADMIN_SECRET,
+      },
+      body: JSON.stringify({
+        query: `
+          query {
+            movies {
+              id
+              title
+              body
+              poster
+            }
+          }
+        `,
+      }),
+    },
+  ).then((response) => response.json());
+
+  return data.movies;
+};
