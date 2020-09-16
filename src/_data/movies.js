b7e6404b55768061864108d2ed0fa92166e4a824
Jason Lengstorf <jason@lengstorf.com>
2020-09-16 09:47:51 -0700

    chore: remove commented out code

--- src/_data/movies.js
+++ src/_data/movies.js
@@ -39,34 +39,4 @@ module.exports = async () => {
   console.log(response);
 
   return response.data.movies;
-  // return [
-  //   {
-  //     id: 1,
-  //     title: 'Test 1',
-  //     poster: 'https://jason.af/fem/img/batman-v-booperman.jpg',
-  //     body:
-  //       'Donec enim set elit donec donec neque eu donec. Fritos elementum gravida adipiscing, sem lacus feugiat convallis nub donec. Tellus mattis gravida, set vel neque ipsum donec ligula donec sem viverra In lacus.',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Test 2',
-  //     poster: 'https://jason.af/fem/img/batman-v-booperman.jpg',
-  //     body:
-  //       'Donec enim set elit donec donec neque eu donec. Fritos elementum gravida adipiscing, sem lacus feugiat convallis nub donec. Tellus mattis gravida, set vel neque ipsum donec ligula donec sem viverra In lacus.',
-  //   },
-  //   {
-  //     id: 3,
-  //     title: 'Test 3',
-  //     poster: 'https://jason.af/fem/img/batman-v-booperman.jpg',
-  //     body:
-  //       'Donec enim set elit donec donec neque eu donec. Fritos elementum gravida adipiscing, sem lacus feugiat convallis nub donec. Tellus mattis gravida, set vel neque ipsum donec ligula donec sem viverra In lacus.',
-  //   },
-  //   {
-  //     id: 4,
-  //     title: 'Test 4',
-  //     poster: 'https://jason.af/fem/img/batman-v-booperman.jpg',
-  //     body:
-  //       'Donec enim set elit donec donec neque eu donec. Fritos elementum gravida adipiscing, sem lacus feugiat convallis nub donec. Tellus mattis gravida, set vel neque ipsum donec ligula donec sem viverra In lacus.',
-  //   },
-  // ];
 };
